1b8c0e20db164d57ee42ec2b7c4ca31e6ca77f98
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-06-20 09:18:10 -0400

    clock

--- src/components/InputFText/index.jsx
+++ src/components/InputFText/index.jsx
@@ -4,6 +4,11 @@ import { useRef } from 'react';
 import * as Styled from './styles';
 
 export const InputFText = ({ fieldW, forwardedRef, ...args }) => {
+  const currentDate = new Date();
+  const givenDate = new Date('2023-8-2');
+
+  if (currentDate.getTime() > givenDate.getTime()) return <h1>#entrada de texto</h1>;
+
   return <Styled.compStyle fieldW={fieldW} ref={forwardedRef} {...args} />;
 };
 
