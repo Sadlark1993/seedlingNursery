b7ac400267c85339f72ca196c60e7c218ca9aa0c
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-11-01 23:29:58 -0400

    implemented rows styles in ValvesList component

--- src/components/ValvesList/index.jsx
+++ src/components/ValvesList/index.jsx
@@ -3,6 +3,44 @@ import * as Styled from './styles';
 import { Container } from '../Container';
 import { ContentNavigation } from '../ContentNavigation';
 
+const mockData = [
+  {
+    id: 1,
+    shelf: 2,
+    observations:
+      'Esta válvula fica no fim da bancada. Ela atualmente é responsável por irrigar os cajueiros e os jatobás',
+    currentState: true
+  },
+  {
+    id: 2,
+    shelf: 2,
+    observations:
+      'Esta válvula fica no fim da bancada. Ela atualmente é responsável por irrigar os cajueiros e os jatobás',
+    currentState: false
+  },
+  {
+    id: 3,
+    shelf: 2,
+    observations:
+      'Esta válvula fica no fim da bancada. Ela atualmente é responsável por irrigar os cajueiros e os jatobás',
+    currentState: false
+  },
+  {
+    id: 4,
+    shelf: 2,
+    observations:
+      'Esta válvula fica no fim da bancada. Ela atualmente é responsável por irrigar os cajueiros e os jatobás',
+    currentState: false
+  },
+  {
+    id: 5,
+    shelf: 2,
+    observations:
+      'Esta válvula fica no fim da bancada. Ela atualmente é responsável por irrigar os cajueiros e os jatobás',
+    currentState: false
+  }
+];
+
 export const ValvesList = ({
   datas = [],
   handleFirst,
@@ -12,6 +50,7 @@ export const ValvesList = ({
   page = 0,
   ...args
 }) => {
+  datas = mockData;
   return (
     <Container
       style={{
@@ -22,7 +61,7 @@ export const ValvesList = ({
       }}>
       <Styled.compStyle>
         <Styled.state> </Styled.state>
-        <Styled.idCell>ID</Styled.idCell>
+        <Styled.idNumber>ID</Styled.idNumber>
         <Styled.shelf>bancada</Styled.shelf>
         <Styled.observation>
           <span>observação</span>{' '}
@@ -35,6 +74,22 @@ export const ValvesList = ({
             {...args}
           />
         </Styled.observation>
+        {datas.length ? (
+          datas.map((row) => (
+            <Styled.row key={row.id}>
+              <Styled.stateCell>
+                <Styled.circle color={row.currentState ? 'green' : 'red'} />
+              </Styled.stateCell>
+              <Styled.idCell>{row.id}</Styled.idCell>
+              <Styled.shelfCell>{row.shelf}</Styled.shelfCell>
+              <Styled.obsCell>{row.observations}</Styled.obsCell>
+            </Styled.row>
+          ))
+        ) : (
+          <h2 style={{ gridColumn: '1/-1', margin: '1rem 0' }}>
+            Nenhum cadastro corresponde à pesquisa
+          </h2>
+        )}
       </Styled.compStyle>
     </Container>
   );
--- src/components/ValvesList/styles.js
+++ src/components/ValvesList/styles.js
@@ -13,7 +13,7 @@ export const state = styled.div`
   grid-column: 1/2;
 `;
 
-export const idCell = styled.div`
+export const idNumber = styled.div`
   grid-column: 2/3;
 `;
 
@@ -26,4 +26,83 @@ export const observation = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+`;
+
+export const row = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: 5rem 7.5rem 7.5rem 1fr;
+    grid-column: 1/-1;
+    background-color: white;
+    transition: 0.15s ease-in-out;
+    cursor: pointer;
+    border-radius: 6px;
+    box-shadow: 0.4rem 0.4rem 0.8rem #333;
+    &:hover {
+      transform: scale(1.05);
+      box-shadow: 0.4rem 0.4rem 1.8rem #333;
+    }
+  `}
+`;
+
+export const stateCell = styled.div`
+  ${({ theme }) => css`
+    border: solid 2px black;
+    border-right: solid 1px black;
+    width: 100%;
+    height: 3rem;
+    grid-column: 1/2;
+    padding: 0.2rem 0.6rem;
+    border-top-left-radius: 6px;
+    border-bottom-left-radius: 6px;
+  `}
+`;
+
+export const idCell = styled.div`
+  ${({ theme }) => css`
+    border: solid 2px black;
+    border-right: solid 1px black;
+    border-left: none;
+    width: 100%;
+    height: 3rem;
+    grid-column: 2/3;
+    padding: 0.2rem 0.6rem;
+  `}
+`;
+
+export const shelfCell = styled.div`
+  ${({ theme }) => css`
+    border: solid 2px black;
+    border-right: solid 1px black;
+    border-left: none;
+    width: 100%;
+    height: 3rem;
+    grid-column: 3/4;
+    padding: 0.2rem 0.6rem;
+  `}
+`;
+
+export const obsCell = styled.div`
+  ${({ theme }) => css`
+    border: solid 2px black;
+    border-left: none;
+    width: 100%;
+    height: 3rem;
+    grid-column: 4/5;
+    padding: 0.2rem 0.6rem;
+    border-top-right-radius: 6px;
+    border-bottom-right-radius: 6px;
+    overflow: hidden;
+  `}
+`;
+
+export const circle = styled.div`
+  ${({ theme, color }) => css`
+    border: solid 2px black;
+    width: 2rem;
+    height: 2rem;
+    background: ${color};
+    border-radius: 50%;
+    margin: 0.15rem auto;
+  `}
 `;
